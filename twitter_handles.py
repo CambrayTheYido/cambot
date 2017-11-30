4b87491c693c2abde03c9768c58804a25f4507c9
CambrayTheYido <ryancambray@hotmail.co.uk>
2017-11-30 21:03:19 +0000

    Added twitter_handles.py

---
+++ twitter_handles.py
@@ -0,0 +1,29 @@
+twitter_handles = {"yotto": "@yottomusic",
+                   "deadmau5": "@deadmau5",
+                   "eric prydz": "@ericprydz",
+                   "dusky": "@duskymusic",
+                   "maduk": "@MadukDnB",
+                   "no mana": "@ihavenomanas",
+                   "pryda": "@ericprydz (Pryda)",
+                   "cirez d": "@ericprydz (Cirez D)",
+                   "tonja holma" : "@ericprydz (Tonja Holma)",
+                   "miike snow": "@miikesnow",
+                   "london grammar": "@londongrammar",
+                   "jme": "@JmeBBK",
+                   "disclosure": "@disclosure",
+                   "chase & status": "@chaseandstatus",
+                   "kendrick lamar": "@kendricklamar",
+                   "madeon": "@madeon",
+                   "sub focus": "@subfocus",
+                   "matt lange": "@MattLange",
+                   "dimension" : "@dimension_uk",
+                   "attlas" : "@attlas"
+                   }
+
+
+def is_artist_in_dict(artist_name):
+    for key, value in twitter_handles.items():
+        if artist_name.lower() == key.lower():
+            artist_name = value
+            break
+    return artist_name
